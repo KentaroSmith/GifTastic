7ff13488f3f841fd6e941069855c5ffc5463dcec
Dan Smith <daniel.smith8795@gmail.com>
2019-07-07 21:53:47 -0500

    set up initial files, found API documentation

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+$(document).ready()
+var apikey = '4P37Mdnvy8QXByKl0NxVYb66NFa0HvNf';
+var topic;
+var trendingURL = "api.giphy.com/v1/gifs/trending?q=" + trendingURL + "&api_key=" + apikey + "&limit=10";
