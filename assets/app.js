20d7f89a3d5fcd81e92703e2e5da7a4c9f8a9726
Dan Smith <daniel.smith8795@gmail.com>
2019-07-09 19:55:17 -0500

    setting up user search function

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,30 @@
 $(document).ready()
-var apikey = '4P37Mdnvy8QXByKl0NxVYb66NFa0HvNf';
-var topic;
-var trendingURL = "api.giphy.com/v1/gifs/trending?q=" + trendingURL + "&api_key=" + apikey + "&limit=10";
+
+$(".topic").on("click", function () {
+    event.preventDefault()
+    var apikey = '4P37Mdnvy8QXByKl0NxVYb66NFa0HvNf';
+    var topic = $(this).attr("data-value");
+    //URL needs to include https:// to query correctly, need to make sure that parameters are searching right
+    var topicURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apikey + "&limit=10&lang=en";
+    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en"
+    $.ajax({
+        url: searchURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < 10; i++) {
+            var gifURL = response.data[i].url;
+            var pics = $("<img src='" + gifURL + "'>");
+            //pics.attr("src", response.data[i].url)
+
+            console.log(gifURL)
+            $("#gif_frame").append(pics)
+
+        }
+
+
+    })
+});
+$(document.body).on("click", "#addtopic", function () {
+    //need to create an addtopic button and input form to add user topics
+})
