4d0f54810e5e1c12809c62394b71932ab5b72d73
Dan Smith <daniel.smith8795@gmail.com>
2019-07-11 21:18:56 -0500

    Updated data values of each button so that they impact the query URL

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,27 @@
 $(document).ready()
-
-$(".topic").on("click", function () {
+//preset buttons that describe the theme - adds a button for each item in the array
+var gallifrey = ["Doctor Who", "Tardis", "Sonic Screwdriver", "Rose Tyler", "Martha Jones", "Donna Nobel", "Amy Pond", "10th Doctor", "11th Doctor", "11th Doctor"]
+for (var i = 0; i < gallifrey.length; i++) {
+    var button = $("<button>" + gallifrey[i] + "</button>");
+    button.addClass("topic");
+    button.attr("data-value", gallifrey[i])
+    $("#buttons").append(button);
+}
+$(document.body).on("click", ".topic", function () {
     event.preventDefault()
     var apikey = '4P37Mdnvy8QXByKl0NxVYb66NFa0HvNf';
     var topic = $(this).attr("data-value");
     //URL needs to include https:// to query correctly, need to make sure that parameters are searching right
     var topicURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apikey + "&limit=10&lang=en";
-    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en"
+    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
     $.ajax({
         url: searchURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
         for (var i = 0; i < 10; i++) {
-            var gifURL = response.data[i].url;
+            topic = gallifrey[i]
+            var gifURL = response.data[i].images.original.url;
             var pics = $("<img src='" + gifURL + "'>");
             //pics.attr("src", response.data[i].url)
 
