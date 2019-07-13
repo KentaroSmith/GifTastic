08be5f476769a917f4d9f540834bdb45babf5564
Dan Smith <daniel.smith8795@gmail.com>
2019-07-12 21:59:52 -0500

    Added in ratings and titles

--- assets/app.js
+++ assets/app.js
@@ -12,7 +12,7 @@ $(document.body).on("click", ".topic", function () {
     var apikey = '4P37Mdnvy8QXByKl0NxVYb66NFa0HvNf';
     var topic = $(this).attr("data-value");
     //URL needs to include https:// to query correctly, need to make sure that parameters are searching right
-    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=10&offset=0&lang=en";
     $.ajax({
         url: searchURL,
         method: "GET"
@@ -20,19 +20,22 @@ $(document.body).on("click", ".topic", function () {
         console.log(response);
         for (var i = 0; i < 10; i++) {
             topic = gallifrey[i]
-            var stillURL = response.data[i].images.original_still.url
+            var stillURL = response.data[i].images.original_still.url;
             var gifURL = response.data[i].images.original.url;
+            var rating = response.data[i].rating;
+            var title = response.data[i].title;
             var pics = $("<img src='" + stillURL + "'>");
+            var title_rating = $("<h4>" + "Rating: " + rating + " Title: " + title + "</h4>")
             pics.attr({
                 "data-animate": gifURL,
                 "data-still": stillURL,
-                "moving": false
+                "moving": false,
+                "rating": rating
             });
             pics.addClass("gif");
             //pics.attr("src", response.data[i].url)
-
             console.log(gifURL);
-            $("#gif_frame").prepend(pics);
+            $("#gif_frame").prepend(pics).prepend(title_rating);
 
         }
 
@@ -59,14 +62,14 @@ $(document.body).on("click", ".gif", function () {
     var moving = $(this).attr("moving");
     if (moving === "false") {
         var startanimate = $(this).attr("data-animate");
-        $(this).attr("src", startanimate)
-        moving = $(this).attr("moving", true)
-        console.log("It's not moving");
+        $(this).attr("src", startanimate);
+        moving = $(this).attr("moving", true);
+        console.log("Now move!");
     }
     else {
         var stopanimate = $(this).attr("data-still");
-        $(this).attr("src", stopanimate)
-        moving = $(this).attr("moving", false)
-        console.log("It's not moving");
+        $(this).attr("src", stopanimate);
+        moving = $(this).attr("moving", false);
+        console.log("Now Stop!");
     }
 })
